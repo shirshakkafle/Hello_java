0a69e521561fe74a67933c6525a8dff32115aea4
shirshak kumar kafle <64686262+shirshakkafle@users.noreply.github.com>
2023-02-06 12:09:42 +0530

    Update Hello.java
    Cancelling my program

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hey monica i am waiting for u");
+        System.out.println("Hey Dit,i am waiting for u");
     }
 }
